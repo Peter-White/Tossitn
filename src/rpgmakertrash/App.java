dde81fa39fb5ad7e69bab889cc2bfa033b48c019
4LeafCoder <pwhitedeveloper@gmail.com>
2018-04-16 12:36:59 -0400

    added create and delete node functions for iterator

--- src/rpgmakertrash/App.java
+++ src/rpgmakertrash/App.java
@@ -55,7 +55,8 @@ public class App {
 			switch (choice) {
 			case 1:
 			case 2:
-				addNode(choice);
+				Node newNode = createNode(choice);
+				addFirst(newNode);
 				break;
 			case 3:
 				addNodeInstructions();
@@ -71,7 +72,7 @@ public class App {
 		}
 	}
 	
-	public static void interateThroughNodes() {
+	public static void iterationThroughNodes() {
 		if(first == null) {
 			System.out.println("Nothing in the drawer.");
 		}
@@ -81,7 +82,8 @@ public class App {
 		} else {
 			position = current;
 		}
-		interationMenu();
+		iterationInstructions();
+		System.out.println("Currently set to: " + current.getData());
 		boolean back = false;
 		while (!back) {
 			System.out.println(position.getData());
@@ -96,12 +98,42 @@ public class App {
 				}
 				break;
 			case 2:
+				System.out.println(current.getData());
+				break;
+			case 3:
 				if(position.getPrevious() != null) {
 					position = position.getPrevious();
 					current = position;
 				} else {
 					System.out.println("Top of the pile reached");
 				}
+				break;
+			case 4:
+				addNode(position, choice);
+				break;
+			case 5:
+				deleteNode(position);
+				if(position.getPrevious() == null) {
+					if(position.getNext() == null) {
+						System.out.println("Your junk drawer is empty. Going back to main.");
+						back = true;
+					} else {
+						position = position.getNext();
+					}
+				} else {
+					position = position.getPrevious();
+				}
+				break;
+			case 6:
+				current = first;
+				System.out.println(current.getData());
+				break;
+			case 7:
+				iterationInstructions();
+				break;
+			case 0:
+				System.out.println("Back to main");
+				back = true;
 				break;
 			default:
 				System.out.println("Not Valid");
@@ -112,7 +144,7 @@ public class App {
 	
 	public static void mainMenuInstructions() {
 		System.out.println("Press: ");
-		System.out.println("1 - To add an item to the drawer");
+		System.out.println("1 - To add an item to the top of the drawer");
 		System.out.println("2 - To print a list of items");
 		System.out.println("3 - To cycle trough items manually");
 		System.out.println("4 - To print instructions");
@@ -125,38 +157,64 @@ public class App {
 		System.out.println("3 - To print instructions");
 		System.out.println("0 - To go back");
 	}
-	public static void interationMenu() {
+	public static void iterationInstructions() {
 		System.out.println("Press: ");
 		System.out.println("1 - To go to the next item");
-		System.out.println("2 - To go back to the previous item");
-		System.out.println("3 - To add a new item");
-		System.out.println("4 - To remove an item");
-		System.out.println("5 - To start from the begining");
-		System.out.println("6 - To print instructions");
+		System.out.println("2 - To see current item");
+		System.out.println("3 - To go back to the previous item");
+		System.out.println("4 - To add a new item");
+		System.out.println("5 - To remove an item");
+		System.out.println("6 - To start from the begining");
+		System.out.println("7 - To print instructions");
 		System.out.println("0 - To go back");
 	}
 	
-	private static void addNode(int i) {
-		Node newNode;
-		if(i == 1) {
-			System.out.println("Enter coin or bill type by dallor value e.g. 0.25 for a quarter");
-			double value = scanner.nextDouble();
-			newNode = new Money(value);
+	private static void addNode(Node node, int choice) {
+		Node newNode = createNode(choice);
+		Node next = node.getNext();
+		if(next == null) {
+			node.setNext(newNode);
+			last = newNode;
+			System.out.println(newNode.getData() + " is at the bottom of the drawer");
 		} else {
-			System.out.println("Enter he name of the object");
-			String title = scanner.nextLine();
-			newNode = new Thing(title);
-		}
-		
-		if(first == null) {
-			addFirst(newNode);
+			next.setPrevious(newNode);
+			node.setNext(newNode);
+			System.out.println(newNode.getData() + " is lying between " + 
+			node.getData() + " and " + next.getData());
 		}
 	}
 	
+	private static void deleteNode(Node node) {
+		boolean done = false;
+		while (!done) {
+			System.out.println("Are you sure you want to delete " + node.getData()
+			 + "? (y/n)");
+			String selection = scanner.nextLine();
+			selection = selection.toLowerCase();
+			switch (selection) {
+				case "y":
+					node.getPrevious().setNext(node.getNext());
+					node.getNext().setPrevious(node.getPrevious());
+					break;
+				case "n":
+					done = true;
+					break;
+				default:
+					System.out.println("Not Valid");
+					break;
+			}
+		}
+
+	}
+	
 	public static void addFirst(Node node) {
-		node.setNext(first);
-		first = node;
-		System.out.println("First node added");
+		if(first == null) {
+			first = node;
+		} else {
+			node.setNext(first);
+			first = node;
+		}
+		System.out.println(node.getData() + " is on top of the pile");
 		if(last == null) {
 			last = node;
 		}
@@ -175,5 +233,19 @@ public class App {
 			current = current.getNext();
 			i++;
 		}
+	}
+	
+	public static Node createNode(int choice) {
+		Node newNode;
+		if(choice == 1) {
+			System.out.println("Enter coin or bill type by dallor value e.g. 0.25 for a quarter");
+			double value = scanner.nextDouble();
+			newNode = new Money(value);
+		} else {
+			System.out.println("Enter he name of the object");
+			String title = scanner.nextLine();
+			newNode = new Thing(title);
+		}
+		return newNode;
 	}
 }
